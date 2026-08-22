151b71551635ecedfe51ff384e44a49e6f3f60a0
usernamekiran <s.kiran88@gmail.com>
2026-08-22 21:22:45 +0530

    + entries to `skip_url_patterns`

--- AMP/amp_supplements.py
+++ AMP/amp_supplements.py
@@ -189,6 +189,7 @@ skip_url_patterns = [
         "bloodybloodybiblecamp.com", "nationalradiochamp.com", "supercamp.com", "miznerparkamp.com", "thijsbroekkamp.com",
         "cio.com", "camp.kg", "adventure-camp.com", "centralfriendscamp.org", "camp.kg",
         "urcamp.edu.br", "cciamp.com", "sturtevantcamp.org", "nextgenchamp.com", "vamp.ee",
+        "workramp.com",
 
     ]
 ]
